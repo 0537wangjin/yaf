3786faf6947cb18a1f2fa5466b97a5e99e206c2b
0537wangjin <0537wangjin@163.com>
2017-09-18 09:40:12 +0800

    require composer

--- public/index.php
+++ public/index.php
@@ -9,6 +9,9 @@ define('APP_PATH', realpath(dirname(__FILE__).DS.'..'.DS.'application').DS);
 define('APPLICATION_PATH', dirname(__FILE__) . '/../');
 define('BASE_URL',"http://localhost:8082/");
 
+// composer
+require_once BASE_PATH.'vendor/autoload.php';
+
 $application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
 
 $application->bootstrap()->run();
