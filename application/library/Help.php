9b958a251bed98ee5bb0f944d3e68784aaaeb655
0537wangjin <0537wangjin@163.com>
2017-10-05 20:28:46 +0800

    Help::randStr

--- application/library/Help.php
+++ application/library/Help.php
@@ -1001,16 +1001,39 @@ class Help
      * @param $length 长度
      * @return string
      */
-    public static function randStr($length)
+    public static function randStr($len = 8, $format = 'ALL')
     {
-        $str = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';//62个字符
-        $strlen = 62;
-        while ($length > $strlen) {
-            $str .= $str;
-            $strlen += 62;
+        $is_abc = $is_numer = 0;
+        $password = $tmp = '';
+        switch ($format) {
+            case 'ALL':
+                $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                break;
+            case 'CHAR':
+                $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+                break;
+            case 'NUMBER':
+                $chars = '0123456789';
+                break;
+            default :
+                $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                break;
         }
-        $str = str_shuffle($str);
-        return substr($str, 0, $length);
+        mt_srand((double)microtime() * 1000000 * getmypid());
+        while (strlen($password) < $len) {
+            $tmp = substr($chars, (mt_rand() % strlen($chars)), 1);
+            if (($is_numer <> 1 && is_numeric($tmp) && $tmp > 0) || $format == 'CHAR') {
+                $is_numer = 1;
+            }
+            if (($is_abc <> 1 && preg_match('/[a-zA-Z]/', $tmp)) || $format == 'NUMBER') {
+                $is_abc = 1;
+            }
+            $password .= $tmp;
+        }
+        if ($is_numer <> 1 || $is_abc <> 1 || empty($password)) {
+            $password = self::randStr($len, $format);
+        }
+        return $password;
     }
 
     /**
