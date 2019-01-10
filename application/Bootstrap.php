43b0c5d494b21d07b837e0d9643dd0706b19c09a
0537wangjin <0537wangjin@163.com>
2019-01-10 16:07:22 +0800

    update

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -12,9 +12,9 @@ class Bootstrap extends Yaf_Bootstrap_Abstract
 
     public function _initConfig()
     {
-        // 把配置保存起来
+        /*// 把配置保存起来
         $this->arrConfig = Yaf_Application::app()->getConfig();
-        Yaf_Registry::set('config', $this->arrConfig);
+        Yaf_Registry::set('config', $this->arrConfig);*/
         // 调用方法 Yaf_Registry::get('config')->application->upyun->bucketname
         // 关闭自动加载模板
         Yaf_Dispatcher::getInstance()->autoRender(FALSE);
@@ -22,56 +22,40 @@ class Bootstrap extends Yaf_Bootstrap_Abstract
 
     // 是否显示错误提示
     public function _initError(Yaf_Dispatcher $dispatcher)
-    {/*{{{*/
-        if ($this->arrConfig->application->db->showErrors) {
+    {
+        if ($this->arrConfig->application->debug) {
             error_reporting(-1);
         } else {
-            error_reporting(-1);
+            error_reporting(0);
         }
     }
 
     // 载入数据库
-    public function _initDatabase()
+    /*public function _initDatabase()
     {
-        // 数据库1
-        Yaf_Registry::set('db', new \Medoo\Medoo([
-            'database_type' => 'mysql',
-            'database_name' => $this->arrConfig->application->db->database,
-            'server' => $this->arrConfig->application->db->hostname,
-            'username' => $this->arrConfig->application->db->username,
-            'password' => $this->arrConfig->application->db->password,
-            'prefix' => $this->arrConfig->application->db->prefix,
-            'logging' => $this->arrConfig->application->db->log,
-            'charset' => 'utf8'
-        ]));
-    }
+
+    }*/
 
     // 载入redis
-    public function _initRedis()
+    /*public function _initRedis()
     {
-        /*$redis = new \Redis();
+        $redis = new \Redis();
         $redis->connect($this->arrConfig->application->redis->host, $this->arrConfig->application->redis->port);
-        Yaf_Registry::set('redis', $redis);*/
-    }
+        Yaf_Registry::set('redis', $redis);
+    }*/
 
-    public function _initPlugin(Yaf_Dispatcher $dispatcher)
+    /*public function _initPlugin(Yaf_Dispatcher $dispatcher)
     {
         //注册一个插件
-    }
+    }*/
 
-    public function _initRoute(Yaf_Dispatcher $dispatcher)
+    /*public function _initRoute(Yaf_Dispatcher $dispatcher)
     {
-
         //在这里注册自己的路由协议,默认使用简单路由
-    }
+    }*/
 
-    public function _initView()
+    /*public function _initView()
     {
         //在这里注册自己的view控制器，例如smarty,twig
-        $loader = new \Twig_Loader_Filesystem('views', APP_PATH);
-        $twig = new \Twig_Environment($loader, array(
-            /* 'cache' => './compilation_cache', */
-        ));
-        Yaf_Registry::set('twig', $twig);
-    }
+    }*/
 }
--- application/controllers/Base.php
+++ application/controllers/Base.php
@@ -8,13 +8,28 @@
  */
 class BaseController extends Yaf_Controller_Abstract
 {
-    protected $twig = null;
+    protected $twig = null;//twig
+    protected $db = null;//db
+    protected $assign = null;//twig模板参数
     /**
      * init 初始化函数
      */
     protected function init()
     {
-        $this->twig = Yaf_Registry::get('twig');
+        $loader = new \Twig_Loader_Filesystem('views', APP_PATH);
+        $this->twig = new \Twig_Environment($loader, array(
+            /* 'cache' => './compilation_cache', */
+        ));
+        $this->db = new \Medoo\Medoo([
+            'database_type' => 'mysql',
+            'database_name' => $this->arrConfig->application->db->database,
+            'server' => $this->arrConfig->application->db->hostname,
+            'username' => $this->arrConfig->application->db->username,
+            'password' => $this->arrConfig->application->db->password,
+            'prefix' => $this->arrConfig->application->db->prefix,
+            'logging' => $this->arrConfig->application->db->log,
+            'charset' => 'utf8mb4'
+        ]);
         // SeasLog 日志设置
         // SeasLog::setBasePath('/data/log');
         // SeasLog::setLogger('kaoqin');
